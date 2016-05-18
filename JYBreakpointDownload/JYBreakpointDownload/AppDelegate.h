6bc3c2b86b5848c66ff6e96ad0e92163c52cb2da
魏景云 <wei_jingyun@outlook.com>
2016-05-18 11:59:36 +0800

    项目的创建

---
+++ JYBreakpointDownload/JYBreakpointDownload/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  JYBreakpointDownload
+//
+//  Created by weijingyun on 16/5/18.
+//  Copyright © 2016年 weijingyun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ JYBreakpointDownload/JYBreakpointDownload/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  JYBreakpointDownload
+//
+//  Created by weijingyun on 16/5/18.
+//  Copyright © 2016年 weijingyun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
