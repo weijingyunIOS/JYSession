637a14cc71f309d4fc602b0e4de7eb748e2f7b1e
魏景云 <wei_jingyun@outlook.com>
2016-05-23 11:58:35 +0800

    更新

---
+++ JYDownLoad/JYDownLoad/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  JYDownLoad
+//
+//  Created by weijingyun on 16/5/23.
+//  Copyright © 2016年 weijingyun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ JYDownLoad/JYDownLoad/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  JYDownLoad
+//
+//  Created by weijingyun on 16/5/23.
+//  Copyright © 2016年 weijingyun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
